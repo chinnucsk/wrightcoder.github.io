03290e318abe0b1a6e3043e56f6e42cb170f309e
Michael Wright <mkwright@gmail.com>
2013-09-19 00:46:48 -0600

    Removed the original part of the site

--- original/assets/js/app.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var wrightCoderApp = angular.module('wrightCoderApp', []);
-
-wrightCoderApp.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/', {templateUrl: 'assets/templates/home.htm', controller: 'homeController'})
-  .otherwise({ redirectTo: '/'});
-}]);
