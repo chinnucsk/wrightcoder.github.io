7d67d18ea3d5282e2b4ce71057fcbca8dc9fe869
Michael Wright <mkwright@gmail.com>
2013-09-20 00:10:24 -0600

    Changed the page to have a better initial home.html page

--- js/services.js
+++ js/services.js
@@ -1,9 +1,17 @@
 'use strict';
 
-/* Services */
-
-
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('blogApp.services', []).
-  value('version', '0.1');
+angular.module('blogApp.services', [], function($provide) { 
+  $provide.factory('postService', function($http, $q) {
+    return {
+      apiPath: '',
+      getAllPosts: function() {
+
+      },
+      getPostDetails: function(postId) {
+        
+      }
+    };
+  });
+}).value('version', '0.2');
